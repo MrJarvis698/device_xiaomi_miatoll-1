b8fda5aadf146293ebf7f384be8a86a6fc03177d
darkbeast69 <guptaaryan189@gmail.com>
2021-02-01 13:43:33 +0000

    miatoll: Decommonized Tree check-in
    there is no no need for common tree if all device is this much identical
    
    also a lil cleanup

---
+++ aosp_miatoll.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2020 The ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from miatoll  device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier
+PRODUCT_NAME := aosp_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM7125
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
