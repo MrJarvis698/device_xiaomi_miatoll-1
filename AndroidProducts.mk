0ef527af8c8ac57a0d3a7b4c45c67da73bcf98b4
Mr.Jarvis698 <dudamajaykumar59@gmail.com>
2021-03-22 11:26:27 +0000

    miatoll: build CorvusOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2020 The CorvusOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_miatoll.mk
+    $(LOCAL_DIR)/corvus_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_miatoll-user \
-    aosp_miatoll-userdebug \
-    aosp_miatoll-eng
+    corvus_miatoll-user \
+    corvus_miatoll-userdebug \
+    corvus_miatoll-eng
--- corvus_miatoll.mk
+++ corvus_miatoll.mk
@@ -7,14 +7,14 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Raven Lair stuff
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := corvus_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM7125
@@ -22,3 +22,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Corvus Official
+RAVEN_LAIR=Official
+USE_GAPPS=true
+
+# Corvus Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.corvus.maintainer=mrjarvis698
